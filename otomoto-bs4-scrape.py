2bb11ee05df24dd9643af89b73ba9cbe975b6a0c
Czaja <dczajka1@gmail.com>
2024-01-24 15:13:51 +0100

    male poprawki, pandas

--- otomoto-bs4-scrape.py
+++ otomoto-bs4-scrape.py
@@ -1,6 +1,5 @@
 from otomoto_funcs.otomoto_functions import scrape_from_page, export_to_csv
 from otomoto_cls.otomoto_classes import SearchConfig
-# import pandas as pd
 try:
     search = SearchConfig()
     offers = scrape_from_page(search.url)
@@ -10,4 +9,3 @@ try:
     export_to_csv(f'{search.make}-{search.model}', offers)
 except ConnectionError:
     print('Connection failure')
-# df = pd.DataFrame()
